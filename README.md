61b2d9aac67f88ab22ff564b935766addf745ac4
Breno Nogueira <breno.s.nogueira@hotmail.com>
2024-04-29 15:45:44 -0300

    Reshaping the repo

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # Data Science Repo
 Me fiddling with data science projects, and learning it!
 
-
-
+I guarantee nothing with this repo. Most of the things here don't fully work or are even finished, so I'd be careful about using any of this code if you don't know what you are doing.
--- aux/dream_catcher.py
+++ aux/dream_catcher.py
@@ -5,11 +5,10 @@ from os.path import join
 
 register_heif_opener()
 
-path = r'C:\Users\Admin\Desktop\images\The Town'
+path = r''
 
-
+# Transforms a HEIC image (goddamnit iphone) to a jpg file
 for file in os.listdir(path):
-
     filename, ext = file.split(".")[0], file.split(".")[-1]
     if ext != "HEIC":
         continue 
